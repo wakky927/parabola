d916767bc27d4cb2041340dd5cb216cf360c76d8
lfc <wakky.927.5831@gmail.com>
2021-11-30 12:10:01 +0900

    終わりましたー！

--- src/half/add_vertical.py
+++ src/half/add_vertical.py
@@ -141,7 +141,7 @@ def load_result():
 def graph_d_180(m_list, mode):
     fig, ax = plt.subplots(figsize=(18, 10))
     plt.xlabel('$\it{U}$ [mm/s]', fontsize=32)
-    plt.ylabel('$\it{m}$ [mm$^{2}/s$]', fontsize=32)
+    plt.ylabel('$\it{m}$ [mm$^{2}$/s]', fontsize=32)
     plt.xticks(fontsize=32)
     plt.yticks(fontsize=32)
     plt.xlim(170, 255)
@@ -178,7 +178,7 @@ def graph_d_180(m_list, mode):
 def graph_d_240(m_list, mode):
     fig, ax = plt.subplots(figsize=(18, 10))
     plt.xlabel('$\it{U}$ [mm/s]', fontsize=32)
-    plt.ylabel('$\it{m}$ [mm$^{2}/s$]', fontsize=32)
+    plt.ylabel('$\it{m}$ [mm$^{2}$/s]', fontsize=32)
     plt.xticks(fontsize=32)
     plt.yticks(fontsize=32)
     plt.xlim(170, 255)
